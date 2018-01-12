9de6d2ebc3531f4c3406d65de4f2245686a7249e
YamiJosema <32712234+YamiJosema@users.noreply.github.com>
2018-01-12 10:03:27 +0100

    Vista inicial

--- principal/views.py
+++ principal/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+#encoding:utf-8
+
+from django.db.models import Max, Count
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+from principal.models import *
+
+def inicio(request):
+    return render_to_response('inicio.html')
