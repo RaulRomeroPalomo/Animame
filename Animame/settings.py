b64e9ebda777bbfc8f8d55ea274e872846c7570b
elvgarrui <elviragruiz@gmail.com>
2018-01-18 01:12:39 +0100

    CSS basico añadido

--- Animame/settings.py
+++ Animame/settings.py
@@ -71,6 +71,8 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(RUTA_PROYECTO, 'static'),
+
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
