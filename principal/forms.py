9808dc4d1ac1b291e42bc2d72b0f319c6bc9b3e6
YamiJosema <32712234+YamiJosema@users.noreply.github.com>
2018-01-18 18:16:08 +0100

    Sistema de recomendación
    Recomendaciones de anime personales por usuario.

--- principal/forms.py
+++ principal/forms.py
@@ -4,7 +4,7 @@ from django import forms
 from principal.models import *
 
 class SearchForm(forms.Form):
-    usuarioId = forms.CharField(label='Nombre de usuario', widget=forms.TextInput, required=True)
+    usuario = forms.CharField(label='Nombre de usuario', widget=forms.TextInput, required=True)
     
 class WordForm(forms.Form):
     word = forms.CharField(label='Palabra de busqueda', widget=forms.TextInput, required=True)
--- principal/views.py
+++ principal/views.py
@@ -1,4 +1,6 @@
 #encoding:utf-8
+import operator
+import shelve
 
 from django.db.models import Max, Count
 from django.shortcuts import render_to_response
@@ -9,6 +11,36 @@ from principal.forms import *
 from index import *
 
 import webscrape
+
+   # matriz de artistas y numero de repeticiones de tags 
+ItemPrefs={}   # matriz de items y puntuaciones de cada usuario. Inversa de Prefs`
+
+def loadDict(usuario):
+#     Caract={}
+    Prefs={}
+#     shelf = shelve.open("dataRS.dat")
+    animes = Anime.objects.all()
+    for row in animes:
+        for tag in row.generos.all():
+            if tag in usuario.topgeneros.all():
+                if row not in Prefs:
+                    Prefs[row]=1
+                else:
+                    Prefs[row]+=1
+        for studio in row.estudios.all():
+            if studio in usuario.topestudios.all():
+                if row not in Prefs:
+                    Prefs[row]=2
+                else:
+                    Prefs[row]+=2
+    
+#     print Prefs  
+    animes = []
+    for row in Prefs:
+        if Prefs[row]>=3:
+            animes.append(row)
+    return animes
+            
 
 def inicio(request):
     return render_to_response('inicio.html')
@@ -23,8 +55,16 @@ def buscar_usuario(request):
     if request.method == 'POST':
         formulario = SearchForm(request.POST)
         if formulario.is_valid():
-            #metodo en cuestion para sacar la infor del usuario
-            return render_to_response('inicio.html')
+            usuario = formulario.cleaned_data['usuario']
+            user, estado = webscrape.getUsuario(usuario)
+            print estado
+            if estado == 0: #Todo bien
+                animes = loadDict(user)
+                return render_to_response('lista.html',{'animes':animes,'usuario':user,'rs':True,'titulo':"Recomendados para '"+usuario+"'"})
+            if estado == 1: #No existe usuario
+                return render_to_response('inicio.html',{'usuario':'No existe usuario'})
+            if estado == 2: #No tiene favoritos
+                return render_to_response('inicio.html',{'usuario':'Debes tener favoritos para usar el sistema de recomendación'})
     else:
         formulario = SearchForm()
     return render_to_response('search.html',{'formulario':formulario,'titulo':'Busca tu usuario'}, context_instance=RequestContext(request))
